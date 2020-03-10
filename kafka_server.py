5e3867c687012aaf1c2e4b325457d8aae977e0b5
Guilherme <GUISIGO@GMAIL.COM>
2020-03-10 20:30:39 -0300

    IMprovements to kafka server

--- kafka_server.py
+++ kafka_server.py
@@ -1,14 +1,17 @@
 import producer_server
 
+TOPIC_NAME = "udacity.police.department.calls.v1"
+SERVER = "localhost:9092"
+CLIENT = "police.department"
 
 def run_kafka_server():
     input_file = "police-department-calls-for-service.json"
 
     producer = producer_server.ProducerServer(
-        input_file=input_file,
-        topic="udacity.police.department.calls",
-        bootstrap_servers="localhost:9092",
-        client_id="police.department"
+        input_file = input_file,
+        topic = TOPIC_NAME,
+        bootstrap_servers = SERVER,
+        client_id = CLIENT
     )
 
     return producer
